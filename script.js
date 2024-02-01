027ed8d07bdfb84b230e1073b5310f78b0c230bd
bonsa <bonsa>
2024-02-01 18:29:39 +0300

    best sitiuation

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 
 
-// Step 1: Create the new element
-// const newElement = document.createElement("div");
-
-// // Step 2: Set attributes or properties (if needed)
-// newElement.textContent = "This is a new element";
-
-// // Step 3: Add a class to the element
-// newElement.classList.add("your-class-name");
-
-// // Step 4: Append the element to a parent element
-// const parentElement = document.querySelector(".parent-element");
-// parentElement.appendChild(newElement);
-
 "use strict"
 const add_btn = document.querySelector(".btn_add");
  const pop_up = document.querySelector(".pop_up");
@@ -39,6 +26,8 @@ function book( name,author, page,read){
          this.read=read;
 }
 
+
+
 function evaluator(){
     add_btn.style.opacity = "0";
     pop_up.style.opacity="100%"
@@ -46,37 +35,28 @@ function evaluator(){
     view_btn.style.opacity="0"  
     container_elements.style.zIndex = "-999";
 }
-view_btn.addEventListener("click",function(event){
-     container_elements.style.opacity="100%"
-     container_elements.style.zIndex = "9999";
-     add_btn.style.opacity="0"
-     view_btn.style.opacity="0"
-     book_collector.forEach(function(book){
-         b_name = book.name;
-         b_author= book.author;
-         b_page=book.page
-         b_state= book.read;
-        
-        let book_index = book_collector.indexOf(book);
-        
-        const top_element =  document.createElement("div");
-        top_element.classList.add("book_container");
-        container_elements.appendChild(top_element);
-      
-        const book_name_element =  document.createElement("div");
+
+function book_name_creater(){
+    const book_name_element =  document.createElement("div");
         book_name_element.classList.add("book_name");
         book_name_element.textContent = b_name;
-        top_element.appendChild(book_name_element);
-
-        const book_author_element =  document.createElement("div");
+        return book_name_element;
+       
+}
+function author_creator(){
+     const book_author_element =  document.createElement("div");
         book_author_element.classList.add("book_author");
         book_author_element.textContent =` by : ${b_author} ` ;
-        top_element.appendChild(book_author_element);
-
-        const book_page_element =  document.createElement("div");
+        return book_author_element;
+}
+function page_creator(){
+    const book_page_element =  document.createElement("div");
         book_page_element.classList.add("book_page");
         book_page_element.textContent =`Page : ${b_page}` ;
-        top_element.appendChild(book_page_element);
+        return book_page_element;
+}
+
+function button_state_creator(){
 
         const button_state= document.createElement("button");
         button_state.classList.add("button_delete");
@@ -90,30 +70,64 @@ view_btn.addEventListener("click",function(event){
             button_state.classList.add("state_closed");
             button_state.textContent = "Not Read";
         }
-        
-        top_element.appendChild(button_state);
-        on_off_btn = document.querySelectorAll(".button_statescript");
-        console.log(on_off_btn)
-        for(let i = 0 ;i<on_off_btn.length;i++) {
-            on_off_btn[i].addEventListener("click",function(){
-            if( button_state.textContent === "Not Read"){
-                button_state.classList.add("state_oppen");
-                button_state.classList.remove("state_closed");
-                button_state.textContent ="Read"
-            }
-            else{
-                button_state.classList.add("state_closed");
-                button_state.classList.remove("state_oppen");
-                button_state.textContent = "Not Read";
-            }
-        });
-        }
-
+        button_state.addEventListener("click",function(){
+                if( button_state.textContent === "Not Read"){
+                                button_state.classList.add("state_oppen");
+                                button_state.classList.remove("state_closed");
+                                button_state.textContent ="Read"
+                            }
+                            else{
+                                button_state.classList.add("state_closed");
+                                button_state.classList.remove("state_oppen");
+                                button_state.textContent = "Not Read";
+                }
+      });
+        return button_state;
+}
+function delete_btn_creator(){
+        let book_index = book_collector.indexOf(book);
         const button_delete_element= document.createElement("button");
         button_delete_element.classList.add("button_delete");
         button_delete_element.setAttribute("value", book_index);
         button_delete_element.textContent = "Delete";
-        top_element.appendChild(button_delete_element);
+        return button_delete_element;
+}
+view_btn.addEventListener("click",function(event){
+     container_elements.style.opacity="100%"
+     container_elements.style.zIndex = "9999";
+     add_btn.style.opacity="0"
+     view_btn.style.opacity="0"
+     book_collector.forEach(function(book){
+         b_name = book.name;
+         b_author= book.author;
+         b_page=book.page
+         b_state= book.read;
+        
+        
+        
+        const top_element =  document.createElement("div");
+        top_element.classList.add("book_container");
+        container_elements.appendChild(top_element);
+    
+        let book_name_element=  book_name_creater()
+        top_element.appendChild(book_name_element);
+
+       let book_author_element = author_creator();
+        top_element.appendChild(book_author_element);
+
+        let book_page_element= page_creator()
+        top_element.appendChild(book_page_element);
+
+      
+        let button_state= button_state_creator();
+        top_element.appendChild(button_state);
+        
+    
+
+        let delete_btn_element = delete_btn_creator();
+
+      
+        top_element.appendChild(delete_btn_element);
 
     
         
@@ -142,7 +156,4 @@ submit_btn.addEventListener("click",function(event){
     view_btn.style.opacity="100%"
     view_btn.style.zIndex = "9999";
 
-
-    
-   
 })
