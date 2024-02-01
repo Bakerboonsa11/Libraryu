e0576f39c85cef381d2f3d9da15673033f35e085
bonsa <bonsa>
2024-02-01 10:12:09 +0300

    js setup

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict"
+const add_btn = document.querySelector(".btn_add");
+ const pop_up = document.querySelector(".pop_up");
+ const name_input = document.querySelector("#name");
+ const author_input = document.querySelector("#author");
+ const page_input = document.querySelector("#page");
+ const cancel_btn = document.querySelector(".cancel_btn");
+ const check_btn = document.querySelector("#check");
+ const submit_btn = document.querySelector(".submit");
+
+const book_collector = [];
+function book( name,author, page,read){
+         this.name=name;
+         this.author=author;
+         this.page=page;
+         this.read=read;
+}
+
+function evaluator(){
+    add_btn.style.opacity = "0";
+    pop_up.style.opacity="100%"
+     
+}
+
+add_btn.addEventListener("click",evaluator)
+
+submit_btn.addEventListener("click",function(event){
+    event.preventDefault()
+    let name_value = name_input.value;
+    let author_value= author_input.value;
+    let page_value= page_input.value;
+    let checkbtn_value = check_btn.value;
+    const newbook= new book(name_value,author_value,page_value,checkbtn_value);
+    book_collector.push(newbook)
+    add_btn.style.opacity = "100%";
+    pop_up.style.opacity="0"
+    
+    console.log(book_collector)
+})
