d61dad8c4bbf71b15d720d17ee08d86984cb0977
bonsa <bonsa>
2024-02-01 14:14:48 +0300

    js opacity

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+
+// Step 1: Create the new element
+// const newElement = document.createElement("div");
+
+// // Step 2: Set attributes or properties (if needed)
+// newElement.textContent = "This is a new element";
+
+// // Step 3: Add a class to the element
+// newElement.classList.add("your-class-name");
+
+// // Step 4: Append the element to a parent element
+// const parentElement = document.querySelector(".parent-element");
+// parentElement.appendChild(newElement);
+
 "use strict"
 const add_btn = document.querySelector(".btn_add");
  const pop_up = document.querySelector(".pop_up");
@@ -30,7 +45,43 @@ view_btn.addEventListener("click",function(event){
      container_elements.style.zIndex = "9999";
      add_btn.style.opacity="0"
      view_btn.style.opacity="0"
-     
+     book_collector.forEach(function(book){
+        let b_name = book.name;
+        let b_author= book.author;
+        let b_page=book.page
+        let b_state= book.read;
+        let book_index = book_collector.indexOf(book);
+        console.log(book_index)
+        const top_element =  document.createElement("div");
+        top_element.classList.add("book_container");
+        container_elements.appendChild(top_element);
+      
+        const book_name_element =  document.createElement("div");
+        book_name_element.classList.add("book_name");
+        book_name_element.textContent = b_name;
+        top_element.appendChild(book_name_element);
+
+        const book_author_element =  document.createElement("div");
+        book_author_element.classList.add("book_author");
+        book_author_element.textContent =` by : ${b_author} ` ;
+        top_element.appendChild(book_author_element);
+
+        const book_page_element =  document.createElement("div");
+        book_page_element.classList.add("book_page");
+        book_page_element.textContent =`Page : ${b_page}` ;
+        top_element.appendChild(book_page_element);
+
+        const button_delete_element= document.createElement("button");
+        button_delete_element.classList.add("button_delete");
+        button_delete_element.setAttribute("value", book_index);
+        button_delete_element.textContent = "Delete";
+        top_element.appendChild(button_delete_element);
+
+
+
+        
+     })
+   
 })
 add_btn.addEventListener("click",evaluator)
 
